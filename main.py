953ffc2aab82094d98851049f004bff9d2e153b5
moldeclubbr-boop <moldeclub.br@gmail.com>
2026-04-05 18:02:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import telebot
-
 bot = telebot.TeleBot("8682954794:AAEmFzNwtikAA-fgkbW8f4UouMLDPUO8n_E")
-
 @bot.message_handler(func=lambda message: True)
-def handle(message):
-    bot.reply_to(message, "Я живой 🔥")
-
+def handle(message):bot.reply_to(message, "Я живой 🔥")
 bot.infinity_polling()
